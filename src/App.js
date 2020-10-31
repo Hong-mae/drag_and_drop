3898f550113c13b34cee6dbbea502b77bd4bf45f
Chill_bi <qkrgksqls9@gmail.com>
2020-10-31 09:04:02 +0900

    drag and drop list 1
    use html drag and drop api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
-function App() {
+const _colors = [
+  'Red',
+  'Orange',
+  'Yellow',
+  'Green',
+  'Blue',
+  'Indigo',
+  'Violet'
+]
+
+var placeholder = document.createElement("li");
+placeholder.className = "placeholder";
+
+const App = () => {
+  const [colors, setColors] = useState(_colors);
+  const [dragged, setDragged] = useState(null);
+  const [over, setOver] = useState(null);
+
+  const dragStart = (e) => {
+    setDragged(e.currentTarget);
+    e.dataTransfer.effectAllowed = 'move';
+    e.dataTransfer.setData('text/html', dragged);
+  }
+
+  const dragEnd = (e) => {
+    dragged.style.display = 'block';
+    dragged.parentNode.removeChild(placeholder);
+
+    // update state
+    var data = colors;
+    var from = Number(dragged.dataset.id);
+    var to = Number(over.dataset.id);
+
+    if (from < to) to--;
+    console.log(data);
+    data.splice(to, 0, data.splice(from, 1)[0]);
+    console.log(data);
+    setColors(data);
+  }
+
+  const dragOver = (e) => {
+    e.preventDefault();
+    dragged.style.display = "none";
+
+    if (e.target.className === 'placeholder') return;
+
+    setOver(e.target);
+    e.target.parentNode.insertBefore(placeholder, e.target);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ul className='list' onDragOver={dragOver}>
+        {
+          colors.map((color, i) => {
+            return (
+              <li
+                data-id={i}
+                key={i}
+                className="list_item"
+                draggable
+                onDragStart={dragStart}
+                onDragEnd={dragEnd}
+              >{color}</li>
+            )
+          })
+        }
+      </ul>
     </div>
   );
 }
